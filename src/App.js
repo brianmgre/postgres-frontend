8007bd727d6976bc621d57ba940442970baa5da1
Brian G <brian.gregorius@gmail.com>
2019-01-16 19:20:20 -0500

    removed the rest of auth

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import "./App.css";
 import Routes from "./components/Routes/Routes.js";
 
 import Toolbar from "./components/Toolbar/Toolbar";
-import SignOut from "./components/SignOut/SignOut.js";
 
 class App extends Component {
   render() {
@@ -14,7 +13,6 @@ class App extends Component {
       <Router>
         <div className="App">
           <Toolbar />
-          <SignOut />
           <Routes className="routes" />
           {/* <Footer /> */}
         </div>
--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -4,20 +4,14 @@ import JobsContainer from "../Landing/jobsContainer";
 import SingleJob from "../Landing/singleJob.js";
 import PostJob from "../postJob/postJob.js";
 import UpdateProfile from "../CompanyProfile/updateProfile.js";
-import SignUp from "../SignUp/SignUp";
-import SignIn from "../SignIn/SignIn";
-import ResetPassword from "../ResetPassword/ResetPassword";
-import Billing from "../billing/billing";
 
 import * as ROUTES from "../../constants/routes";
-import './Routes.css';
+import "./Routes.css";
 
 class Routes extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      userAuth: null
-    };
+    this.state = {};
   }
 
   render() {
@@ -27,11 +21,7 @@ class Routes extends Component {
         <Route path={ROUTES.JOB} render={props => <SingleJob {...props} />} />
         <Route path={ROUTES.POST_JOB} component={PostJob} />
         {/* <Route path={ROUTES.COMPANY_PROFILE} component={Profile} /> */}
-        <Route path={ROUTES.BILLING} component={Billing} />
-        <Route path={ROUTES.SIGN_UP} component={SignUp} />
-        <Route path={ROUTES.SIGN_IN} component={SignIn} />
-        <Route path={ROUTES.RESET_PASSWORD} component={ResetPassword} />
-        <Route path={ROUTES.ACCOUNT} component = {UpdateProfile} />
+        <Route path={ROUTES.ACCOUNT} component={UpdateProfile} />
       </div>
     );
   }
